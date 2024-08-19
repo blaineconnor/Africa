9dc07d9e64ca479ae2d6de23312f0c365626453b
fsarman <fsarman@ahlatciteknoloji.com.tr>
2024-08-19 14:36:45 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Fatih Emre Sarman on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
